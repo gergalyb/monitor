347382bdc949e65f86af724cce15b82256472901
gergalyb <gergalyb@gmail.com>
2015-07-02 13:50:03 +0200

    github stahp

--- views/test.php
+++ views/test.php
@@ -8,21 +8,21 @@ require './config/conn.php';
 
 echo "<div class='view'>";
 
-echo "<form method='post' action=" . $_SERVER['PHP_SELF'] . ">
+echo "<form method='get' action=" . $_SERVER['PHP_SELF'] . ">
     <input type='text' name='date' placeholder='yyyy-mm-dd'>
     <input type='submit' name='submit'>
   </form>";
 
-if (!empty($_POST['submit'])) {
-  $date = $_POST['date']; //to be sanitized!
+if (!empty($_GET['submit'])) {
+  $date = $_GET['date']; //to be sanitized!
   $sql = "select COUNT(cTriggerName) as Darab, cState as Status, right(cTriggerName,2) as DepotCode
   from BNDOCEX2TLOG
   where
-  StartTime > '$date' and
-  StartTime < dateadd(d,1,'$date')
+  StartTime > ? and
+  StartTime < dateadd(d,1,?)
   group by right(cTriggerName,2) , cState";
 
-  $stmt = sqlsrv_query($conn, $sql);
+  $stmt = sqlsrv_query($conn, $sql, array($date, $date));
   if ($stmt === false) {
   die(print_r(sqlsrv_errors(), true));
   }
