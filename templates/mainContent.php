e29810e3e2bda0ae77ebfb32306b7ae4e9486054
gergalyb <gergalyb@gmail.com>
2015-07-02 13:41:39 +0200

    Revert "started implementing excel export"
    This reverts commit ebc5b706b3a727730eeb3658ab0516aa029d2f83.

--- templates/mainContent.php
+++ templates/mainContent.php
@@ -1,32 +1,17 @@
 <?php
-require './libraries/php-excel.class.php';
+//table generation
 
+//if global result array/object has data, generate table
 echo "<div id='mainContent'>";
 echo "info: mainContent <br />";
 
 if (!empty($_SESSION['result'])) { //not working correctly
-    $exportRaw = $_SESSION['result'];
-    echo "<form method='post' action=" . $_SERVER['PHP_SELF'] . ">";
-        echo "<input type='button' value='Letöltés' name='export'>";
-    echo "</form>";
-    echo "<table id='table'>";
-        echo "<tr>";
-            echo "<th>" . "Darab" . "</th>";
-            echo "<th>" . "Status" . "</th>";
-            echo "<th>" . "DepotCode" . "</th>";
-        echo "</tr>";
-        while ($row = sqlsrv_fetch_array($_SESSION['result'], SQLSRV_FETCH_ASSOC)){
-            echo "<tr>";
-                echo "<td>".$row['Darab']."</td><td>".$row['Status']."</td><td>".$row['DepotCode']."</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-        if (!empty($_POST['export'])) {
-            //fill export array
-        }
-}   elseif ($_SESSION['result'] == 0) {
-        echo "<b>Nincs találat!</b>";
-}   else {
-        echo "<b>Az eredmények itt fognak megjelleni</b>";
+  while ($row = sqlsrv_fetch_array($_SESSION['result'], SQLSRV_FETCH_ASSOC)){
+    echo $row['Darab'] . ", " . $row['Status'] . ", " . $row['DepotCode'] . "<br />";
+  }
+} elseif ($_SESSION['result'] == 0) {
+  echo "<b>Nincs találat!</b>";
+} else {
+  echo "<b>Az eredmények itt fognak megjelleni</b>";
 }
 echo "</div>";
