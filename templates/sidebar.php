c181553d78a72db365e0524d2b9410b46e734164
gergalyb <gergalyb@gmail.com>
2015-07-09 17:32:26 +0200

    accordion working

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -5,12 +5,12 @@ function makeAccordionElement($title, $id, $content){
     echo "<div class='panel panel-default'>
     <div class='panel-heading' role='tab' id='$id'>
       <h4 class='panel-title'>
-        <a role='button' data-toggle='collapse' data-parent='#accordion' href='#$id' aria-expanded='true' aria-controls='$id'>
+        <a role='button' data-toggle='collapse' data-parent='#accordion' href='#A$id' aria-expanded='true' aria-controls='A$id'>
           $title
         </a>
       </h4>
     </div>
-    <div id='$id' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='$id'>
+    <div id='A$id' class='panel-collapse collapse' role='tabpanel' aria-labelledby='$id'>
       <div class='panel-body'>
         $content
       </div>
@@ -40,60 +40,27 @@ foreach ($queries as $objQuery) {
         }
         $contentNormal = $contentNormal . "<input type='submit' class='btn btn-flat btn-default' name=" . $objQuery->name . ">";
         $contentNormal = $contentNormal . "</form>";
-        //var_dump($contentNormal);
+    }
 
+    if ($objQuery->category == "quick"){
+        $contentQuick = $contentQuick . "<form class='form-horizontal jumbotron' method='POST' action=" . $_SERVER['PHP_SELF'] . ">";
+        foreach ($paramsKeys as $key) {
+            if ($objQuery->params[$key] == "date"){
+                $contentQuick = $contentQuick . "<div class='date-input'>";
+                    $contentQuick = $contentQuick . "<label for=" . $key . ">" . $key . "</label>";
+                    $contentQuick = $contentQuick . "<input type='text' type='text' class='form-control' id='date-input' name=" . $key . " id=" . $key . ">";
+                $contentQuick = $contentQuick . "</div>";
+            }
+            else {
+                echo "error";
+            }
+        }
+        $contentQuick = $contentQuick . "<input type='submit' class='btn btn-flat btn-default' name=" . $objQuery->name . ">";
+        $contentQuick = $contentQuick . "</form>";
     }
 }
+makeAccordionElement("quick", "quick", $contentQuick);
 makeAccordionElement("normal", "normal", $contentNormal);
     echo "</div>";
-    ?>
-
-    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingOne">
-      <h4 class="panel-title">
-        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </a>
-      </h4>
-    </div>
-    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingTwo">
-      <h4 class="panel-title">
-        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
-        </a>
-      </h4>
-    </div>
-    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingThree">
-      <h4 class="panel-title">
-        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </a>
-      </h4>
-    </div>
-    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
-
-
-    <?php
 echo "</div>";
 ?>
